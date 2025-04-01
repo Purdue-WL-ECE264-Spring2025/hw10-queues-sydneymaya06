c54d0643f496a19c279bb84885e6f3baed97241a
sydneymaya06 <sydneymaya06@gmail.com>
2025-04-01 14:35:48 -0400

    Search for shortest path to solved board

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,84 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) 
+{ 
+  struct list_node * nd = malloc(sizeof(struct list_node));
+  nd -> value = value;
+  nd -> next = NULL;
+  return nd; 
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value)
+{
+  struct list_node * ptr = new_node(value);
+  ptr -> next = list->head;
+  list->head = ptr;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value)
+{
+  //printf("insert at tail\n");
+  // printf("val: %d\n", (int)value);
+  // printf("address %p\n", list->head);
+  if(list->head == NULL)
+  {
+    // printf("list is NULL\n");
+    list->head = new_node(value);
+    return;
+  }
+  struct list_node * p = list->head;
+  //printf("%d\n", (int)p->value);
+  while((p->next != NULL))
+  {
+    p = p->next;
+  }
+  p -> next = new_node(value);
+  return;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) 
+{ 
+  if(list->head == NULL)
+  {
+    return -1;
+  }
+  struct list_node * p = list->head;
+  size_t val = p->value;
+  list->head = list->head->next;
+  free(p);
+  
+  return val; 
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list)
+{ 
+  if(list->head == NULL)
+  {
+    return -1;
+  }
+  struct list_node * p = list->head;
+  struct list_node * q;
+  while((p->next != NULL))
+  {
+    q = p;
+    p = p->next;
+  }
+  q -> next = NULL;
+  size_t val = p->value;
+  free(p);
+  return val; 
+}
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) 
+{
+  while(list.head != NULL)
+  {
+    struct list_node * p = list.head->next;
+    free(list.head);
+    list.head = p;
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,128 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) 
+{
+    insert_at_tail(&q->data, serialize(state));
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) 
+{ 
+    struct game_state board;
+    board = deserialize(remove_from_head(&q->data));
+    return board; 
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) 
+{ 
+    //printf("serialized: %d\n", (int)serialize(start)); // 87097344
+    struct queue q;
+    int count = 0;
+    int tier = 1;
+    int queue_count = 0;
+    q.data.head = NULL;
+    int success = 0;
+    enqueue(&q, start);
+    printf("queue item: %ld\n", q.data.head->value);
+    //enqueue(&q, start);
+    //printf("queue item: %d\n", (int)q.data.head->next->value);
+    //size_t val;
+    struct game_state next_grid; // = dequeue(&q);
+    
+    
+
+    while(success == 0)
+    {
+        // int length = 1;
+        // struct list_node * p = q.data.head;
+        // //printf("%d\n", (int)p->value);
+        // while((p->next != NULL))
+        // {
+        //     p = p->next;
+        //     length++;
+        // }
+        // //printf("length: %d\n", length);
+        for(int ind = 0; ind < tier; ind++)
+        {
+            next_grid = dequeue(&q);
+            if(serialize(next_grid) == 81985526993846272)
+            {
+                success = 1;
+                printf("SUCCESS");
+                break;
+            }
+            struct game_state grid_copy = next_grid;
+        // printf("next grid: %d, %d\n", next_grid.empty_col, next_grid.empty_row);
+        // printf("next grid: %d\n", next_grid.tiles[next_grid.empty_row + 1][next_grid.empty_col]);
+        // printf("next grid: %d\n", next_grid.tiles[next_grid.empty_row][next_grid.empty_col]);
+
+        // MOVE UP
+        if(next_grid.empty_row < 3)
+        {
+            next_grid.tiles[next_grid.empty_row][next_grid.empty_col] = next_grid.tiles[next_grid.empty_row + 1][next_grid.empty_col];
+            next_grid.tiles[next_grid.empty_row + 1][next_grid.empty_col] = 0;
+            enqueue(&q, next_grid);
+            queue_count++;
+            //printf("up ");
+        }
+
+        // MOVE DOWN
+        if(next_grid.empty_row > 0)
+        {
+            next_grid = grid_copy;
+            next_grid.tiles[next_grid.empty_row][next_grid.empty_col] = next_grid.tiles[next_grid.empty_row - 1][next_grid.empty_col];
+            next_grid.tiles[next_grid.empty_row - 1][next_grid.empty_col] = 0;
+            enqueue(&q, next_grid);
+            queue_count++;
+            //printf("down ");
+    
+        }
+        
+        // MOVE LEFT
+        if(next_grid.empty_col < 3)
+        {
+            next_grid = grid_copy;
+            next_grid.tiles[next_grid.empty_row][next_grid.empty_col] = next_grid.tiles[next_grid.empty_row][next_grid.empty_col + 1];
+            next_grid.tiles[next_grid.empty_row][next_grid.empty_col + 1] = 0;
+            enqueue(&q, next_grid);
+            queue_count++;
+            //printf("left ");
+        }
+
+        // MOVE RIGHT
+        if(next_grid.empty_col > 0)
+        {
+            next_grid = grid_copy;
+            next_grid.tiles[next_grid.empty_row][next_grid.empty_col] = next_grid.tiles[next_grid.empty_row][next_grid.empty_col - 1];
+            next_grid.tiles[next_grid.empty_row][next_grid.empty_col - 1] = 0;
+            enqueue(&q, next_grid);
+            queue_count++;
+            //printf("right ");
+        }
+        
+        
+        }
+        if(success == 1)
+        {
+            printf("\nCOUNT: %d\n", count);
+            break;
+        }
+        printf("tier %d\n", tier);
+        tier = queue_count;
+        printf("tier %d\n", tier);
+        queue_count = 0;
+        count++;
+        printf("count %d\n", count);
+        if(count > 11)
+        {
+            break;
+        }
+    }
+        
+    //     if(count == 2)
+    //     {
+    //         break;
+    //     }
+    // }
+    return count; 
+}
